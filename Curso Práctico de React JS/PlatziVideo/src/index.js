3715edfee32e93006ee1b7c73bddda40695a6141
Daniel Lozano Campano <danlozcam@gmail.com>
2021-03-10 23:18:06 +0100

    do the thing

--- Curso Práctico de React JS/PlatziVideo/src/index.js
+++ Curso Práctico de React JS/PlatziVideo/src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const element = <App />;
+const container = document.getElementById('app');
+
+ReactDOM.render(element, container);
